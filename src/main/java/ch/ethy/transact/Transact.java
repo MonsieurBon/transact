bd45921e15ccd82d94e5d1a45118752cb3073566
Fabian Gut <fabian.gut@ethy.ch>
2021-07-23 23:24:09 +0200

    Refactored main class

--- src/main/java/ch/ethy/transact/Transact.java
+++ src/main/java/ch/ethy/transact/Transact.java
@@ -9,25 +9,40 @@ import java.util.*;
 import static ch.ethy.transact.server.HttpMethod.*;
 
 public class Transact {
-  public static final String WEBAPP_BASE_PATH_OPTION = "--webappBasePath";
-  public static final String WEBAPP_BASE_PATH_DEFAULT = "/webapp";
+  private static final Logger LOG = Logger.getLogger(Transact.class);
+
+  private static final String WEBAPP_BASE_PATH_OPTION = "--webappBasePath";
+  private static final String WEBAPP_BASE_PATH_DEFAULT = "/webapp";
+  private static final String SERVER_SECRET_OPTION = "--serverSecret";
+  private static final String SERVER_SECRET_DEFAULT = "secret";
+
   private RequestHandler staticResourcesHandler;
 
   public static void main(String[] args) {
     Transact app = new Transact();
 
-    String webappBasePath = Arrays.stream(args)
-        .dropWhile(arg -> !WEBAPP_BASE_PATH_OPTION.equals(arg))
-        .limit(2)
-        .skip(1)
-        .findFirst()
-        .orElse(WEBAPP_BASE_PATH_DEFAULT);
+    String webappBasePath = getArgumentValue(args, WEBAPP_BASE_PATH_OPTION, WEBAPP_BASE_PATH_DEFAULT);
+    String serverSecret = getArgumentValue(args, SERVER_SECRET_OPTION, SERVER_SECRET_DEFAULT);
 
-    app.setup(webappBasePath);
+    try {
+      app.setup(webappBasePath, serverSecret);
+    } catch (Exception e) {
+      LOG.fatal("Failed to setup application", e);
+      System.exit(1);
+    }
     app.run();
   }
 
-  private void setup(String webappBasePath) {
+  private static String getArgumentValue(String[] args, String webappBasePathOption, String webappBasePathDefault) {
+    return Arrays.stream(args)
+        .dropWhile(arg -> !webappBasePathOption.equals(arg))
+        .limit(2)
+        .skip(1)
+        .findFirst()
+        .orElse(webappBasePathDefault);
+  }
+
+  private void setup(String webappBasePath, String serverSecret) {
     Logger.addAppender(new ConsoleAppender());
 
     staticResourcesHandler = new StaticResourcesHandler(webappBasePath);
